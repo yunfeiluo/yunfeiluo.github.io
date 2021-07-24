7fd9b5a50683edd91d4328cd96df561a05290505
yunfeiluo <2630064129@qq.com>
2021-07-24 17:49:10 -0400

    update docs name

--- static/js/docs.js
+++
@@ -1,8 +0,0 @@
-function fetch_projects(){
-    fetch('https://api.github.com/repos/yunfeiluo/yunfeiluo.github.io/git/trees/master')
-    .then(res => res.json())
-    .then((out) => {
-        console.log('Output: ', out);
-    }).catch(err => console.error(err));
-    alert("complete");  
-}

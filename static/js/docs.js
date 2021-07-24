13cd383555bf5ddd927200b0e2ada96aa41d47fa
yunfeiluo <2630064129@qq.com>
2021-07-23 23:53:20 -0400

    test_for_fetch

--- static/js/docs.js
+++ static/js/docs.js
@@ -1,7 +1,7 @@
-function fetch_projects(){
-    await octokit.request('GET https://api.github.com/repos/{owner}/{repo}/git/trees/master', {
+async function fetch_projects(){
+    const result = await octokit.request('GET https://api.github.com/repos/{owner}/{repo}/git/trees/master', {
         owner: 'yunfeiluo',
         repo: 'yunfeiluo.github.io'
       })
-    // alert(files);
+    alert(result);
 }

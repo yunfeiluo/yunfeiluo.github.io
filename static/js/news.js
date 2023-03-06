b3fcd6855f4919947708eafd6eed361a83858870
yunfeiluo <2630064129@qq.com>
2023-03-05 23:53:41 -0500

    separate news section #5

--- static/js/news.js
+++ static/js/news.js
@@ -5,7 +5,7 @@ class News extends React.Component {
 // <li>[date] content.</li>
     make_news(){  
         return (
-            <ul style="list-style-type:square">
+            <ul>
                 <li>[March 2023] My first workshop paper "Agent Performing Autonomous Stock Trading under Good and Bad Situations" by me and <a href="https://www.linkedin.com/in/zhangqi-duan-4140311b6/" target="_blank">Zhangqi Duan</a> has been accepted in <a href="https://ai4abm.org/" target="_blank">AI4ABM</a> at <a href="https://iclr.cc/Conferences/2023" target="_blank">ICLR 2023</a>. More details will be updated soon!</li>
                 <li>[Feburary 2023] Received Ph.D. offer from <a href="https://datascience.ucsd.edu/" target="_blank">Halıcıoğlu Data Science Institute</a> at <a href="https://ucsd.edu/" target="_blank">University of California San Diego</a>! Advised by <a href="https://www.tauhidurrahman.com/" target="_blank">Prof. Tauhidur Rahman</a>.</li> 
                 <li>[June 2022] Joined the summer internship of Data Science for Common Good at <a href="https://ds.cs.umass.edu/" target="_blank">Center for Data Science - UMass Amherst</a>, working with RedCross on the project of satellite imagery based computer vision research project.</li>

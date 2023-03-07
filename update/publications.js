5ed8685cabbde44607e0954ea49e260c4c118bac
yunfeiluo <2630064129@qq.com>
2023-03-07 09:38:42 -0500

    fix small errors

--- update/publications.js
+++ update/publications.js
@@ -1,4 +1,4 @@
 var online_list = {
     // "name of article": "link to article",
-    "[publication] Test article. More content will be updated soon!": "error.html"
+    "Test article. More content will be updated soon!": "error.html"
 };

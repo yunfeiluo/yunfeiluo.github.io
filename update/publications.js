e66f67052dc53dd433c6842f60cf4c89a80ef613
yunfeiluo <2630064129@qq.com>
2024-06-24 23:22:07 +0800

    update publications

--- update/publications.js
+++ update/publications.js
@@ -1,4 +1,6 @@
 var online_list = {
     // "name of article": "link to article",
-    "Agent Performing Autonomous Stock Trading under Good and Bad Situations": "https://arxiv.org/abs/2306.03985"
+    "[Jun 2024, NAACL SRW] Start Simple: Progressive Difficulty Multitask Learning": "https://aclanthology.org/2024.naacl-srw.7/",
+    "[Mar 2024, Nature Scientific Report] Dynamic clustering via branched deep learning enhances personalization of stress prediction from mobile sensor data": "https://www.nature.com/articles/s41598-024-56674-2",
+    "[Jun 2023, ICLR AI4ABM] Agent Performing Autonomous Stock Trading under Good and Bad Situations": "https://arxiv.org/abs/2306.03985"
 };
